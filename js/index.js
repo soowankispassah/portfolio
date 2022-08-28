91f5c568258123a4b4adb3ed5501f3a9af6e7e7e
soowankispassah <74257838+soowankispassah@users.noreply.github.com>
2022-08-28 13:02:37 +0530

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+const navBody = document.querySelectorAll('.nav')
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
+navBody.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+})
